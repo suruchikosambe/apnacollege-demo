47a529d2a8fd34573837d6a1a6eabc10e5e00865
suruchikosambe <suruchikosambe@gmail.com>
2025-08-18 10:01:12 +0530

    modifying paras.py

--- paras.py
+++ paras.py
@@ -1,3 +1,4 @@
 print("Hello world \n hello paras sunil kosambe.")
 print("hello i am practicing to become a data scientist")
 print("my full name is paras sunil kosambe")
+print("my repo name is apnacollege-demo")
